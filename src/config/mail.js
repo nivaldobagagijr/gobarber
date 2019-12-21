6bb08040cf1dc1f14897320c00e29840e2f1c4d2
Nivaldo Bagagi Jr <bagagi.junior@gmail.com>
2019-12-21 13:50:19 -0300

    Configurando Nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'c5068a45d64482',
+    pass: '864dbf4163801e',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
